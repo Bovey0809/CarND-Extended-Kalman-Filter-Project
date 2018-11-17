d57b5b881b09268a1a3cd651ff2cc55f0e9a1245
Bovey0809 <my364007886@gmail.com>
2018-11-17 21:23:00 +0900

    test git

--- src/tools.cpp
+++ src/tools.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "tools.h"
 
+
 using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
@@ -15,6 +16,21 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd RMSE(4);
+  RMSE << 0, 0, 0, 0;
+  int n = estimations.size();
+  if(estimations.size() != ground_truth.size() || estimations.size() == 0){
+    return RMSE;
+  }
+  
+  for(int i=0; i<n; i++){
+    VectorXd residue = estimations[i] - ground_truth[i];
+    residue = residue.array() * residue.array();
+    RMSE += residue; 
+  }
+  RMSE = RMSE / n;
+  RMSE = RMSE.array().sqrt();
+  return RMSE;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
@@ -22,4 +38,18 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   TODO:
     * Calculate a Jacobian here.
   */
+  MatrixXd Hj(3, 4);
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  float j = px*px + py*py;
+  float j1 = sqrt(j);
+  float j2 = j * j1;
+  
+  Hj << px/j1, py/j1, 0, 0,
+    -py/j, px/j, 0, 0,
+    py*(vx*py - vy * px)/j2, px*(vy*px-vx*py)/j2, px/j1, py/j1;
+  return Hj;
 }
